39a6f42a6df0e4f4611345220b3c3b6dc3965559
freemandealer <freeman.zhang1992@gmail.com>
2024-11-22 11:01:18 +0800

    update: literally ONE-click
    Signed-off-by: freemandealer <freeman.zhang1992@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 A Google Chrome Extension to copy info (summary, link, etc) from JIRA page to ClipBoard.
 
-Go to chrome://extensions/ and use `Load unpacked` to load this extention from filesystem.
+Go to chrome://extensions/, enable `developer mode` and use `Load unpacked` to load this extention from filesystem.
 
--- popup.js
+++ popup.js
@@ -1,3 +1,36 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const [summaryResult, linksResult] = await Promise.all([
+      chrome.scripting.executeScript({
+        target: { tabId: (await chrome.tabs.query({ active: true, currentWindow: true }))[0].id },
+        func: () => document.querySelector('h1#summary-val').innerText
+      }),
+      chrome.scripting.executeScript({
+        target: { tabId: (await chrome.tabs.query({ active: true, currentWindow: true }))[0].id },
+        func: () => Array.from(document.querySelectorAll('a.issue-link'), link => link.href)
+      })
+    ]);
+
+    const summaryText = summaryResult[0].result;
+    const issueLinks = linksResult[0].result;
+
+    const combinedText = `${summaryText} ${issueLinks.join('\n')}`;
+
+    await navigator.clipboard.writeText(combinedText);
+    document.body.innerHTML = '<p>Copied!</p>';
+    console.log('Combined text copied to clipboard:', combinedText);
+    setTimeout(() => {
+      window.close();
+    }, 1500);
+  } catch (error) {
+    document.body.innerHTML = '<p>Error copying text</p>';
+    console.error('Error copying combined text to clipboard:', error);
+    setTimeout(() => {
+      window.close();
+    }, 1500);
+  }
+});
+
 document.getElementById('copyButton').addEventListener('click', async () => {
   try {
     const [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true });
